83930da359348f6dc00cb0ae57d60a066e994cef
Atharva <atharva.deshmukh@siemens.com>
2024-01-30 21:19:55 +0530

    initial commit

---
+++ BitManipulation/evenOdd.ts
@@ -0,0 +1,30 @@
+// Checking Odd or Even
+
+/* Logic:
+
+either divide by 2 and check for remainder 
+OR
+check using the least significant bit(LSB)
+
+LSB --> Least Significant Bit --> Righmost bit since any change in this causes very less significant change
+MSB --> Most Significant Bit --> Leftmost bit since any change in this causes a very significant change
+
+0 -->  0000
+1 -->  0001
+2 -->  0010
+3 -->  0011
+4 -->  0100
+5 -->  0101
+6 -->  0110
+7 -->  0111
+
+so (even) & (0001) -> 0
+*/
+
+function checkEvenOdd(n: number): boolean {
+    if(n & 1) return true;
+    return false;
+}
+
+console.warn(checkEvenOdd(12));
+console.warn(checkEvenOdd(1));

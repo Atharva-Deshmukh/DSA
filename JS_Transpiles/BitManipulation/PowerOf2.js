642cf2e4e394b94a417bdc601950f3ab4f39303b
Atharva <atharva.deshmukh@siemens.com>
2024-01-30 22:34:53 +0530

    PowerOfTwo

---
+++ JS_Transpiles/BitManipulation/PowerOf2.js
@@ -0,0 +1,23 @@
+/*
+Check if a number is power of 2
+
+/* Logic: A power of two in binary has only one '1' bit.
+
+2 -->  0010
+4 -->  0100
+8 -->  1000
+
+If you subtract 1 from a power of two, you get a binary number with all '1' bits.
+1 -->  0001
+3 -->  0011
+7 -->  0111
+
+so n & (n-1) -> 0
+Hence power of two
+*/
+function powOfTwo(n) {
+    return ((n & (n - 1)) === 0);
+}
+console.warn(powOfTwo(2));
+console.warn(powOfTwo(3));
+console.warn(powOfTwo(128));

088f43671a8bd3b46f2e3132e680a5dfa1491e3b
Atharva <atharva.deshmukh@siemens.com>
2024-02-10 18:32:47 +0530

    stacks using classes

---
+++ Stacks/Stack_using_Class.ts
@@ -0,0 +1,40 @@
+class Stack {
+    private size: number;
+    private currentSize: number;
+    private stack: number[];
+
+    constructor(size: number) {
+        this.size = size;
+        this.currentSize = 0;
+        this.stack = [];
+    }
+
+    push(element: number) {
+
+        if(this.currentSize === this.size) alert('STACK IS FULL');
+        else {
+            this.stack[this.currentSize] = element;
+            this.currentSize++;
+        }
+    }
+
+    pop() {
+        if(this.currentSize === 0 ) alert('STACK IS EMPTY');
+        else  {
+            this.currentSize--;
+            this.stack.length = this.currentSize;
+        }
+    }
+
+    display() {
+        console.warn(this.stack);
+    }
+};
+
+let myStack = new Stack(5);
+myStack.push(1); 
+myStack.push(2); 
+myStack.push(3); 
+myStack.push(4); 
+myStack.pop(); 
+myStack.display(); 

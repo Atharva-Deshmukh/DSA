83d5c02cbef67e80f42cb3b2a3da0fab83671e4e
Atharva <atharva.deshmukh@siemens.com>
2024-02-10 21:54:20 +0530

    circular queue added

--- JS_Transpiles/Queues/CircularQueue.js
+++ JS_Transpiles/Queues/CircularQueue.js
@@ -1,59 +1,54 @@
-/* REFER THE IMAGE TO KNOW WHY DO WE USE A CICRULAR QUEUE 
+/* REFER THE IMAGE TO KNOW WHY DO WE USE A CICRULAR QUEUE
 Since, in a normal queue, which is an array under the hood, we cannot add further elements
 even though we have space at front, since our pointer has reached the end,
 we use circular queue here
 
-We use array of specific size here since we can add a new element by first removing any 
+We use array of specific size here since we can add a new element by first removing any
 existing one, so Its MEMORY EFFICIENT
 */
-
 class CircularQueue {
-    private cqueue: number[];
-    private size: number;
-    private currentSize: number;
-    private front: number;
-    private rear: number;
-
-    constructor(size: number) {
+    cqueue;
+    size;
+    currentSize;
+    front;
+    rear;
+    constructor(size) {
         this.size = size;
-
         this.cqueue = [];
-        this.front = -1;   // denotes index here
+        this.front = -1; // denotes index here
         this.rear = -1;
         this.currentSize = 0;
     }
-
-    enqueue(val: number) {
-
+    enqueue(val) {
         // agar deuque full nhi hai to
-        if(this.currentSize !== this.size) {
-            
+        if (this.currentSize !== this.size) {
             // agar queue ka size max tak pohoch gya, to rear ko 0th index, uski khudki jagah 
             // pe leke aajao, rear jab yaha aayga to now, hum circular way se handle kar sakte
             //Note that rear is an index, hence size-1
-            if(this.rear === this.size-1) this.rear = 0;
-            else this.rear++;
-
+            if (this.rear === this.size - 1)
+                this.rear = 0;
+            else
+                this.rear++;
             this.cqueue[this.rear] = val;
             this.currentSize++;
-
             // if first element hai, to front ko bhi 0 pe leke aao jaha tab rear hoga
-            if(this.front === -1) this.front = this.rear;
+            if (this.front === -1)
+                this.front = this.rear;
         }
         else {
             alert('QUEUE IS FULL');
         }
     }
-
     dequeue() {
-        if(this.currentSize !== 0) {
+        if (this.currentSize !== 0) {
             // delete current element at front (front = 0) at start when we did this.front = this.rear
             this.cqueue[this.front] = null;
-
             // element remove honey ke baad front jo hai vo array me aage aage aata hai, 
             // but if uska index max size barabar hai, to usko 0 pe laao in order to reuse the space
-            if(this.front === this.size-1) this.front = 0;
-            else this.front++;
+            if (this.front === this.size - 1)
+                this.front = 0;
+            else
+                this.front++;
             this.currentSize--;
         }
         else {
@@ -63,12 +58,11 @@ class CircularQueue {
             alert('QUEUE IS EMPTY');
         }
     }
-
     display() {
         console.warn(this.cqueue);
     }
-};
-
+}
+;
 let cq = new CircularQueue(4);
 cq.enqueue(1);
 cq.enqueue(2);
@@ -76,7 +70,3 @@ cq.enqueue(3);
 cq.display();
 cq.dequeue();
 cq.display();
-
-
-
-

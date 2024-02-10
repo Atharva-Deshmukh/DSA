eb3d6985637347e27be27b1382387dce7309b0ea
Atharva <atharva.deshmukh@siemens.com>
2024-02-10 18:10:05 +0530

    stacks using functions

---
+++ JS_Transpiles/Stacks/Stack_using_Functions.js
@@ -0,0 +1,26 @@
+/* Internally, it is implemented as an array only
+We can use Array.pop() to take out the last element and Array.push() to add element to the last
+But then, it wouldn't be a data structure.
+
+Similarly, for a queue, we can use Array.unshift() to add element in the beginning and
+Array.pop() to pop element from the end.
+*/
+let stack = [];
+let currentSize = stack.length;
+function push(element) {
+    stack[currentSize] = element; // ensures we always add element to the last index only
+    currentSize++;
+}
+function pop() {
+    if (currentSize === 0)
+        alert('Stack is EMPTY');
+    currentSize--;
+    stack.length = currentSize; // free the memory of last element
+}
+push(1);
+push(2);
+push(3);
+push(4);
+push(5);
+pop();
+console.warn(stack);

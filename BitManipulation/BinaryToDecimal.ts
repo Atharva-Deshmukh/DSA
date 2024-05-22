d0138ee97831a0268aaf5326c4323125850392fe
Atharva <atharva.deshmukh@siemens.com>
2024-05-22 22:47:35 +0530

    binary to decimal way

--- BitManipulation/BinaryToDecimal.ts
+++ BitManipulation/BinaryToDecimal.ts
@@ -8,11 +8,26 @@ TC: O(logn) since approx bits in a number is logn
 SC: O(1)
 */
 
+// A little niave way
 function binaryToDecimal(binary: string): number {
     let decimal: number = 0;
 
     for(let i = binary.length - 1; i >= 0; i--) {
         if(binary[i] === '1') decimal += Math.pow(2,i);
+    }
+
+    return decimal;
+}
+
+// this can be made faster
+function binaryToDecimalFaster(binary: string): number {
+    let decimal: number = 0;
+    let pow_2 = 1; // since 1 to sabhi me add hoga, 2 ^ 0 to hoga hi sab me
+
+    for(let i = binary.length - 1; i >= 0; i--) {
+        if(binary[i] === '1') decimal += pow_2;
+        pow_2 = pow_2 << 1 ; //n = n * 2, left me iterate karte time double hote jaygega
+                             // keep left shifting 1 by 1, it becomes power of 2 
     }
 
     return decimal;

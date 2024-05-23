0cfc24cf3e49fb9419a0b575e8508b36dab87715
Atharva <atharva.deshmukh@siemens.com>
2024-05-23 20:11:15 +0530

    2s Complement

---
+++ BitManipulation/2sComplement.ts
@@ -0,0 +1,59 @@
+/* 2’s complement of a binary number is 1, added to the 1’s complement of the binary number. 
+In the 2’s complement representation of binary numbers, the MSB represents the sign with a ‘0’ used 
+for plus sign and a  ‘1’ used for a minus sign. the remaining bits are used for representing magnitude.  
+
+2's complement of "0111" is  "1001"        ||         2's complement of "1100" is  "0100"
+
+
+Way 1: Obvious
+- get ones complement and add 1 to it
+
+Way 2: New!!
+- Start from the Least Significant Bit and traverse left until you find a 1.  
+  Until you find 1, the bits stay the same
+- Once you have found 1, let the 1 as it is, and now
+- Flip all the bits left into the 1.
+
+EXAMPLE => FIND 2s Complement of 100100
+
+- Traverse and let the bit stay the same until you find 1. Here x is not known yet. Answer = xxxx00 –
+- You found 1. Let it stay the same. Answer = xxx100
+- Flip all the bits left into the 1. Answer = 011100.
+  Hence, the 2s complement of 100100 is 011100.
+
+
+TC: O(len)
+SC: O(1)
+
+0 1 2 3 
+let rightmost set index = 1
+no of 0 bits = 2 = (4 - 1 - 1) OR (binary.length - rightMostSetBitPositionIndex - 1)
+
+0 1 2 3 4 5 6 7
+let rightmost set index = 5
+no of 0 bits = 2 = (8 - 2 - 1) OR (binary.length - rightMostSetBitPositionIndex - 1)
+*/
+
+let rightMostSetBitPositionIndex = (binary: string): number => {
+    for(let i = binary.length-1; i >= 0; i--) {
+        if(binary[i] === '1') return i;
+    }
+    return -1;
+}
+
+function TwosComplement(binary: string): string {
+    let twosComp: string = '';
+
+    // get rightmost set bit index and keep it as it is
+    let rightMostSetBitIndex: number = rightMostSetBitPositionIndex(binary);
+
+    // add number of 0s after rightMost set bit in 2s complement
+    twosComp = '1' + '0'.repeat(binary.length - rightMostSetBitIndex - 1);
+
+    // now invert all bits after this
+    for(let i = rightMostSetBitIndex-1; i >= 0; i--) {
+        twosComp = ((binary[i] === '1')? '0': '1') + twosComp;
+    }
+
+    return twosComp;
+}

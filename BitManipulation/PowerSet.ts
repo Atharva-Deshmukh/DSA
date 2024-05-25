9228411fe27430dcaf1156bd6d25f2063a4defbf
Atharva <atharva.deshmukh@siemens.com>
2024-05-25 15:47:14 +0530

    power set

--- BitManipulation/PowerSet.ts
+++ BitManipulation/PowerSet.ts
@@ -1,18 +1,38 @@
-/* 
-Power set P(S) of a set S is the set of all subsets of S. For example S = {a, b, c} 
+/* Power set P(S) of a set S is the set of all subsets of S. For example S = {a, b, c} 
 then P(s) = {{}, {a}, {b}, {c}, {a,b}, {a, c}, {b, c}, {a, b, c}}.
 If S has n elements in it then P(s) will have 2^n elements.
 
 Note that in Binary representation of nos from [0-7], we have the power set
-0 -->  0000      {}
-1 -->  0001      {c}
-2 -->  0010      {b}
-3 -->  0011      {b, c}
-4 -->  0100      {a}
-5 -->  0101      {a, c}
-6 -->  0110      {a, b}
-7 -->  0111      {a, b, c}
+0 -->  000    {}
+1 -->  001    {c}
+2 -->  010    {b}
+3 -->  011    {b, c}
+4 -->  100    {a}
+5 -->  101    {a, c}
+6 -->  110    {a, b}
+7 -->  111    {a, b, c}
 
+SIMILARLY, if for every power of 2, if we iterate from 0 to 2^n-1, we find the bits are in proper power set permutation
+
+0 -->  00000
+1 -->  00001
+2 -->  00010
+3 -->  00011
+4 -->  00100
+5 -->  00101
+6 -->  00110
+7 -->  00111
+8 -->  01000
+9 -->  01001
+10 --> 01010
+11 --> 01011
+12 --> 01100
+13 --> 01101
+14 --> 01110
+15 --> 01111
+16 --> 10000 
+
+LETS GET back to problem,
 here if string = "abc" n=3, 
 u will require bits till [1 to n]
 

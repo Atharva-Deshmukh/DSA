f4a2a14c51afb4fd41045c3e518a726725bc566e
Atharva <atharva.deshmukh@siemens.com>
2024-08-17 14:26:29 +0530

    Toggle ith bit from right

---
+++ BitManipulation/7.Toggle_ith_Bit.ts
@@ -0,0 +1,26 @@
+/* N = 5, K = 2  Output: 7
+
+5 => 0 1 0 1
+7 => 0 1 1 1
+
+ORIGIN = right.
+
+Logic: 
+- do 1 << (k-1) since k is absolute pos and 1 is already at pos = 1 
+
+  let k = 4 and n = 100010
+  1 0 0 0 1 0
+  0 0 1 0 0 0
+
+- now XOR, since for same bits XOR = 0 and for different bits XOR = 1
+    1 0 0 0 1 0
+  ^ 0 0 1 0 0 0
+    -----------
+    1 0 1 0 1 0  so kth bit toggled + rest of the 1s preserved
+
+TC: O(1)
+SC: O(1) */
+
+function Toggle_ith_Bit_FromRight(n: number, k: number): number {
+    return (n ^ (1 << (k-1)));
+}

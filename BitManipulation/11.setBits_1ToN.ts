fa57b85606b17e185034e6038c0425ed01b864d9
Atharva <atharva.deshmukh@siemens.com>
2024-08-20 22:24:45 +0530

    set bits from 1 to N

--- BitManipulation/11.setBits_1ToN.ts
+++ BitManipulation/11.setBits_1ToN.ts
@@ -56,7 +56,7 @@ before 8 (0-7) set of 1 is complete for first 3 bits
 3 -->  0011  in second last row, we have (8/2) set bits, --> 4 
 4 -->  0100  in third last row, we have (8/2) set bits,  --> 4
 5 -->  0101
-6 -->  0110   Total No of Set Bits till 2^3 =>  3*(2^(3-1)) -> 12 
+6 -->  0110   Total No of Set Bits till 2^3 =>  3*(2^(3-1)) -> 12  --> x*(2^(x-1))
 7 -->  0111
 
 Now after the highest power of 2,
@@ -71,12 +71,12 @@ Now after the highest power of 2,
 for remaining bits, see the bits other than MSB, it becomes a subproblem now: 
 000  --> 0
 001  --> 1
-010  --> 2                        SUBPROBLEM NOW ->   subproblem(N - 2^x)
+010  --> 2                        SUBPROBLEM NOW ->  Solve(N - 2^x)
 011  --> 3
 100  --> 4  highest power now
 101  --> 5
 
-before 2's power                --> 4/2 + 4/2 --> 2^(2-1)*2;
+before 2's power                --> 4/2 + 4/2 --> 2*(2^(2-1));
 after 2's power                 --> (5-4)+1
             
 subproblem now:
@@ -84,40 +84,46 @@ subproblem now:
 00  --> 0
 01  --> 1  --> this is highest power of 2, --> 2^0  BASE CASE HAI YE
 
-so overall solution using recursion: for N , if highest power of 2 is x, then
+so overall solution using recursion: 
+for N , if highest power of 2 is x, then
+total set bits from 1 to N = [ x*(2^(x-1)) + (N-2*x)+1 ] keep recursing till u get 1 as highest power of 2 */
 
-total set bits from 1 to N = [ (2^(x-1)*x) + (N-2*x+1) ] keep recursing till u get 1 as highest power of 2
-*/
+
+/*  3 2 1 0
+9 - 1 0 0 1         pos of MSB = 4, highest power of 2 = 3 = pos - 1
+
+    3 2 1 0
+5 - 0 1 0 1         pos of MSB = 3, highest power of 2 = 2 = pos - 1
+
+    3 2 1 0
+3 - 0 0 1 1         pos of MSB = 2, highest power of 2 = 1 = pos - 1  */
 
 function highestPow2TillN(n: number): number {
 
-    // get position of MSB of n
-    if(n < 0) return -1;
-    if(n >= 0 && n <= 2) return n; 
+    //corner cases
+    if (n <= 1) return 0;
+    if (n === 2) return 1;
 
     let pos = 0;
-    let temp=n;   //preserving n
+    let temp = n; // preserve n
 
-    while(temp) {
+    while (temp) {
         temp = temp >> 1;
         pos++;
     }
 
-    return pos-1;
+    return pos - 1;
 }
-// console.warn('highest power of 2 before 3-> ', highestPow2TillN(3));
 
 function totalSetBits(N: number): number {
+    if (N === 0) return 0;
+    if (N === 1) return 1;
 
-    console.warn('N -> ', N);
-
-    if(N === 0 || N === 1) return N;
-
-    //highest power till before N
     let x = highestPow2TillN(N);
-    console.warn('Math.pow(2,x) -> ', Math.pow(2,x));
 
-    return ((x * Math.pow(2,x-1)) + (N - Math.pow(2,x) + 1)) + totalSetBits(N - Math.pow(2,x)); 
+    return (x * Math.pow(2, x - 1)) + (N - Math.pow(2, x) + 1) + totalSetBits(N - Math.pow(2, x));
+
 }
 
-console.warn('Total set bits -> ', totalSetBits(7));
+
+

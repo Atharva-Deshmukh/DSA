f61e4dfc2114a25b7a1ccec0ca149ca4f8c334d8
Atharva <atharva.deshmukh@siemens.com>
2024-08-20 22:36:35 +0530

    copy set bits in a range

--- BitManipulation/12.copySetBitsInARange.ts
+++ BitManipulation/12.copySetBitsInARange.ts
@@ -8,7 +8,7 @@ Input  : x = 10, y = 13, l = 2, r = 3
 Output : x = 14
 10 --> 1010 
 13 --> 1101. 
-There is one set bit in y at 3'rd position (in given range). 
+There is one set bit in y at 3'rd position (from right). 
 After we copy this bit to x, x becomes 1110 --> 14
 
 Input  : x = 8, y = 7, l = 1, r = 2
@@ -20,8 +20,9 @@ Output : x = 11
 Way 1: Traverse both nums at a time from LSB to MSB and set the required bits in range [l, r]
 TC: O(logN) SC: O(1)
 
-Way 2: Make a mask and AND it with first number to get the set bits in that range
-and then OR it with the second number to set the bits that were unset and keep set ones as it is.
+Way 2: Use mask
+Create a mask and AND it with x.
+and then OR it with y to set the bits that were unset and keep set ones as it is.
 
 IMPORTANT CONCEPT -> 
 The bit mask that has set bits in the range [L, R] can be generated using:
@@ -39,7 +40,7 @@ function createMaskInRange(L: number, R: number): number {
 }
 
 
-function copySetBitsInARange(A: number, B: number, L: number, R: number): number {
+function copySetBitsInARange(x: number, y: number, L: number, R: number): number {
 
     // corner case
     if((L < 1) || (R > 32)) return NaN;
@@ -47,16 +48,11 @@ function copySetBitsInARange(A: number, B: number, L: number, R: number): number
     let mask = createMaskInRange(L, R);
 
     // get all set bits from B in range [L-R]
-    mask = mask & B;
+    mask = mask & y;
 
     // OR the mask with A to set the unset bits and keep set bits as it is
-    A = A | mask;
+    x = x | mask;
 
-    return A;
+    return x;
 }
-
-console.warn('A after flipping -> ', copySetBitsInARange(10, 13, 2, 3));
-console.warn('A after flipping -> ', copySetBitsInARange(8, 7, 1, 2));
-console.warn('A after flipping -> ', copySetBitsInARange(16, 2, 1, 3));
-console.warn('A after flipping -> ', copySetBitsInARange(44, 3, 1, 5));
 

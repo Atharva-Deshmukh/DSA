0416d06616964902a29d1cc1122e3d7b21af46b8
Atharva <atharva.deshmukh@siemens.com>
2024-08-21 21:34:55 +0530

    Decimal to Binary

--- BitManipulation/14.DecimalToBinary.ts
+++ BitManipulation/14.DecimalToBinary.ts
@@ -1,13 +1,27 @@
-/* Input : n = 10 Output : 1010
+/* n = 10 ==>  1010
+
+TRICK:
+ex: n = 10  
+let binary = ""
+
+ 10 % 2 = 0  binary = 0
+
+ 5 % 2 = 1   binary = 10
+
+ 2 % 2 = 0   binary = 010
+
+ 1 % 2 = 1   binary = 1010
+
+ 0 --> STOP
+
 - keep dividing n by 2 & noting the remainder
-- keep directly attaching that remainder to answer string
-- stop when n is 1
+- keep directly attaching that remainder to answer string at the end
+- stop when n is 0
 - print in reverse order
 - Do every operation in bit operators, so that execution is fast
 
 TC: O(logn) since approx bits in a number is logn
-SC: O(1)
-*/
+SC: O(1) */
 
 function decimalToBinary(n: number): string {
     let binary: string = '';

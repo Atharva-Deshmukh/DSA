1de8180b68407ad6767276c0708d33493dbee281
Atharva <atharva.deshmukh@siemens.com>
2024-08-23 21:57:33 +0530

    XOR in a range

--- BitManipulation/XOR_in_range.ts
+++ BitManipulation/XOR_in_range.ts
@@ -1,7 +1,6 @@
 import { XOR_1ton } from "./18.XOR_from_1toN";
 
-/* 
-Input: L = 4, R = 8                 Output: 8 
+/*  Input: L = 4, R = 8                 Output: 8 
 4 ^ 5 ^ 6 ^ 7 ^ 8 = 8
 
 Input: L = 3, R = 7                 Output: 3  
@@ -14,16 +13,14 @@ TC: O(R-L+1)
 SC: O(1) 
 
 Way 2: Utilise XOR from 1 to n since we get that in O(1)
-- we can easily get XOR from 1 to N using a function done before.
+- we can easily get XOR_1ton() done before.
 - let L = 4, and R = 7
   (1 ^ 2 ^ 3) ^ (1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 ^ 7), so 1, 2 and 3 gets cancelled since a ^ a = 0
 
   so XOR_1_n(L - R) = XOR_1_n(L - 1) ^ XOR_1_n(R)
 
 TC: O(1)
-SC: O(1)
-
-*/
+SC: O(1) */
 
 function XOR_L_R(L: number, R: number): number {
     return XOR_1ton(L-1) ^ XOR_1ton(R);    

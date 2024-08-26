a17abe3ac3511fc98c619cf0bdcea4584fc6d6dd
Atharva <atharva.deshmukh@siemens.com>
2024-08-26 20:39:31 +0530

    ones complement

--- BitManipulation/OnesComplement.ts
+++ BitManipulation/OnesComplement.ts
@@ -1,13 +1,28 @@
 /* 1’s complement of a binary number is another binary number obtained by toggling all bits in it, i.e., 
-transforming the 0 bit to 1 and the 1 bit to 0.In the 1’s complement format , the positive numbers remain unchanged. 
-The negative numbers are obtained by taking the 1’s complement of positive counterparts.
+[0 -> 1] && [1 -> 0]
 
-for example +9 will be represented as 00001001 in eight-bit notation and -9 will be represented as 11110110, 
-which is the 1’s complement of 00001001. 
+For positive numbers, the 1's complement is simply a bit-flipped version of the binary number.
+
+Ex:
+
+            +5 ->  0101
+1's Complement ->  1010
+
+For negative numbers, the 1's complement is used as a way to represent the negative value in binary systems.
+We don't practically calculate 1s complement for a negative number.
+
+Ex:
+
+            +5 ->  0101
+            -5 ->  1010  
+
+SIGNIFICANCE OF 1s complement:
+- To represent negative numbers
+- Simplifying Subtraction Addition. When you subtract a number, you can add its 1's complement 
+  instead of performing direct subtraction.
 
 TC: O(len)
-SC: O(1)
-*/
+SC: O(1) */
 
 let flipBit = (char: string): string => (char === '0')? '1': '0';
 

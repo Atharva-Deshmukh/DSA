383712e160182613161158b93468435eafeb124f
Atharva <atharva.deshmukh@siemens.com>
2024-08-26 21:56:14 +0530

    twos complement

--- BitManipulation/21.TODO_TwosComplement.ts
+++ BitManipulation/21.TODO_TwosComplement.ts
@@ -1,4 +1,6 @@
-/* 2’s complement =  1 + 1’s complement
+/*DO THIS:  https://leetcode.com/problems/sum-of-two-integers/description/ 
+
+2’s complement =  1 + 1’s complement
 
 Significance: It is also used to store negative numbers
 

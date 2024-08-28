bc46a82458aefaa4d4f9f625834226ca89909c9d
Atharva <atharva.deshmukh@siemens.com>
2024-08-28 21:54:51 +0530

    single number 2

--- BitManipulation/25.singleNumber2.ts
+++ BitManipulation/25.singleNumber2.ts
@@ -6,8 +6,8 @@ Input: nums = [0,1,0,1,0,1,99]      Output: 99
 
 Way 1: 
 - use map to store frequency and return element with frequency one but this uses an extra space
-TC: O(logn)
-SC: O(1) */
+TC: O(length of array)
+SC: O(no.of.unique elements) */
 
 // just to revise Map
 function singleNumberMapSolution(arr: number[]): number {
@@ -29,12 +29,7 @@ function singleNumberMapSolution(arr: number[]): number {
     return -1;
 }
 
-/* For Map wala solution
-TC: O(length of array)
-SC: O(no.of.unique elements) */
-
-/*
-Way 2:
+/* Way 2:
 - We can see an observation related to bit manipulation
 - let arr = [5,5,5,6,4,4,4]
 
@@ -61,19 +56,21 @@ Way 2:
 - We can see that at max we need to iterate from [0th bit to 31st bit]
 
 TC: O(len * 32)
-SC: O(1)
-*/
+SC: O(1)*/
 
 function singleNumberBitSolution(a: number[]): number {
     let ans: number = 0;
 
-    for(let bitIndex = 0; bitIndex < 32; bitIndex++) {
+    for(let bitPosition = 0; bitPosition < 32; bitPosition++) {
         let count: number = 0;
-        a.forEach((num) => {
-            if(num & (1 << bitIndex)) count++;
+
+        // for this bit position, count how many numbers have that bit position set
+        a.forEach((ele) => {
+            if(ele & (1 << bitPosition)) count++;
         });
 
-        if(count%3) ans = ans | (1 << bitIndex);
+        // set that bit in the answer, OR is perfect as 0 ko 0 and 1 ko 1
+        if(count % 3) ans = ans | (1 << bitPosition);
     }
 
     return ans;
@@ -81,12 +78,10 @@ function singleNumberBitSolution(a: number[]): number {
 
 /* OPTIMISED further using the CONCEPT OF BUCKETS, Its a MUST MEMORISE solution (Cannot derive during interview)
 
-let arr = [2,2,2,1]
+maintain three buckets = 0 initially
 
-I will maintain 3 buckets for storing numbers as per their count in the array
-
-bucket 1 => ones
-bucket 2 => twos
+bucket 1 => ones for elements with count 1
+bucket 2 => twos for elements with count 2
 bucket 3 => threes
 
 Note that, 
@@ -94,17 +89,23 @@ Note that,
 - If the element is in ones already, it will go to twos
 - If the element is in twos already, it will go to threes
 
-Now, we need to think of a bitwise operator we can use to add and remove elements as per out need in these buckets
-from &, | and ^, & can't be used since it is not used for adding something, | or ^ can be used, but 
+Now, we need to think of a bitwise operator we can use to add and remove elements as per our need from these buckets.
+From (&, | and ^), & can't be used since it is not used for adding something, | or ^ can be used, but 
 let twos = 2 is present, now we need to remove this 2 from twos bucket and add this into threes, this can't be done using |
 since 2 | 2 will again add 2 in twos, instead 2 ^ 2 will remove this from twos and 0 ^ 2 will add 2 in threes
-So, we will be using XOR operator 
+So finally, we will be using XOR operator 
+
+DRY RUN:  let arr = [2,2,2,1]
 
 - initially i = 0, the first element will ALWAYS be in ones bucket so ones = 0 ^ 2 = 2
+b1 = 2
+b2 = 0
+b3 = 0
+
 - i = 1, if element is not there in twos, it will go to ones, how to represent that, twos is 0 initially, so ~twos = 1
   ((nums[i] ^ ones) & (~twos))
   if it was in twos already, let nums[i] = 2 = 1 0
-  So, (0 ^ 0 1) & ~(0 1) => (0 1) & (1, 0) = 0, So, u don't push this element in twos, u push it in threes
+  So, (1 0 ^ 1 0) & ~(0 0) => (0 0) & (1 1) = 0, So, u don't push this element in twos, u push it in threes
 
   similarly, if the number is to be added in twos, it should be there in ones
   (nums[i] ^ twos) & (~ones)
@@ -114,8 +115,7 @@ So, we will be using XOR operator
   return ones finally
 
   TC: O(len)
-  SC: O(1)
-*/
+  SC: O(1) */
 
 function singleNumberBucketSolution(a: number[]): number {
     let ones: number = 0;
@@ -123,10 +123,10 @@ function singleNumberBucketSolution(a: number[]): number {
 
     a.forEach((num) => {
         // add it to ones if it is not in twos, for the next element in the array, if the element is same 
-        //  this ones line will automatically remove the element from ones since ones ^ ones = 0 and 0 & 0 = 0
+        // this ones line will automatically remove the element from ones since ones ^ ones = 0 and 0 & 0 = 0
         ones = (num ^ ones) & (~twos);
 
-        // add it to twos if it is there is ones, but here we already removed it from ones, so this line works fine
+        // add it to twos if it is there in ones, but here we already removed it from ones, so this line works fine
         twos = (num ^ twos) & (~ones);
     });
 

19cfc4b70922b4dfcb2b210e2c4ff3b0f6d5ae81
Atharva <atharva.deshmukh@siemens.com>
2024-09-04 21:33:10 +0530

    count set bits + leetcode

--- BitManipulation/4.countSetBits.ts
+++ BitManipulation/4.countSetBits.ts
@@ -71,3 +71,84 @@ function countSetBits2(n: number) {
 console.warn('way 1 Iterative-->', countSetBits1_Iterative(3));
 console.warn('way 1 Recursive-->', countSetBits1_Recursive(3));
 console.warn('way 2 -->', countSetBits2(3));
+
+
+/* LEETCODE QUESTION: 
+338. Counting Bits
+
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), 
+ans[i] is the number of 1's in the binary representation of i.
+
+Example 1: => Input: n = 2     Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+
+Example 2: => Input: n = 5     Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+ 
+It is very easy to come up with a solution with a runtime of O(n log n). 
+Can you do it in linear time O(n) and possibly in a single pass?
+
+Approach 1: Naive
+
+- Corner cases for 0 and 1,
+- Iterate every num and count set bits using BK Algo
+
+TC: O(n * log(num))
+SC: O(1)
+
+Approach 2: Observe a pattern
+
+0 --> 0000          6 --> 0110                
+1 --> 0001          7 --> 0111
+2 --> 0010          8 --> 1000
+3 --> 0011          9 --> 1001
+4 --> 0100         10 --> 1010
+5 --> 0101         11 --> 1011
+
+When n is even, no of bits in n === no of bits in n/2
+ex: 6 - 2 | 8 - 1
+    3 - 2 | 4 - 1
+
+When n is odd, no of bits in n === (no of bits in n/2 + 1)
+ex: 7 - 3 | 9 - 2
+    3 - 2 | 4 - 1
+
+- we can keep storing the halves in a dp[]
+- store basic counts like 0, 1 and 2 in dp[0, 1, 1], index = number itself
+- start from 3 now, 
+   3 === odd, 3/2 == 1 => dp[1] + 1 =>  dp[0, 1, 1, 2], store this as well in the array
+
+- since this dp[] is count[] itself, return it lastly
+
+TC: O(n)
+SC: O(n) */
+
+function countBits(n: number): number[] {
+    // corner cases
+    if(n === 0) return [0];
+    if(n === 1) return [0, 1];
+    if(n === 2) return [0, 1, 1];
+    
+    let ans: number[] = [0, 1, 1];
+
+    for(let num = 3; num <= n; num++) {
+        if((num % 2) === 0) {
+            let count: number = ans[Math.floor(num / 2)];
+            ans.push(count);
+        } else {
+            let count: number = ans[Math.floor(num / 2)] + 1;
+            ans.push(count);
+        }
+    }
+
+    return ans;
+}

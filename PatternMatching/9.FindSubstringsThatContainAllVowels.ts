bf5280d27ceff41935fd9b22c7411fa5c4dd29b4
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-01-12 15:38:40 +0530

    find substrings that contain all vowels

--- PatternMatching/9.FindSubstringsThatContainAllVowels.ts
+++ PatternMatching/9.FindSubstringsThatContainAllVowels.ts
@@ -12,10 +12,12 @@ Naive Approach: (a simple two pointer approach), this is the only possible appro
 - check if it has all the vowels only or not using a hash
 - this will take O(n ^ 3) => O(n ^ 2) for generating all possible substrings and O(n) to verify all vowels
 
-- We can slightly optmise it to O(n ^ 2): have two loops i and j
-- i will denote starting character of substring and j will keep adding char to substring generated ONLY if it is a vowel
+- We can slightly optmise it to O(n ^ 2): have two loops i and j, adding char to substring only if it is a vowel
+- i will denote starting character of substring and j will keep adding char to substring generated ONLY 
+  if it is a vowel
 - when vowel count = 5, print/push that substring
-- when we encounter consonant, just i++ to change the start of substring.
+- when we encounter consonant, just i++ to change the start of substring. 
+  Now we will see other substring combinations with the new start
 
 TC: O(n ^ 2)
 SC: O(1) */
@@ -53,60 +55,5 @@ function findAllVowelSubstringNaiveApproach(s: string): string[] {
         }
     }
 
-    return ans;
-}
-
-
-
-
-
-
-
-
-*/
-
-function findAllVowelSubstringSlidingWindowApproach(s: string): string[] {
-    let n: number = s.length;
-
-    // corner case
-    if(n < 5) return [];
-
-    let ans: string[] = [];
-    let i: number = 0;
-    let j: number = 0;
-
-    // keep generating a new hash for every substring
-    let vowelHash: Map<string, number> = new Map<string, number>();
-    let vowelSubstring: string = "";
-
-    while((i < n) && (j < n)) {
-        if(isVowel(s[j]) === false) {
-            i++;
-            j = i;
-            vowelHash.clear();
-            vowelSubstring = "";
-        }
-        else {
-            vowelSubstring += s[j];
-            if(!vowelHash.has(s[j])) vowelHash.set(s[j], 1);
-            else vowelHash.set(s[j], vowelHash.get(s[j])!+1);
-
-            // whenever map size reaches 5, push that substring
-            if(vowelHash.size === 5) {
-                ans.push(vowelSubstring);
-
-                // Remove the first character to shrink the window
-                // Shrink the window from the left
-                let leftChar = s[i];
-                vowelHash.set(leftChar, vowelHash.get(leftChar)! - 1);
-                if (vowelHash.get(leftChar) === 0) {
-                    vowelHash.delete(leftChar);
-                }
-                vowelSubstring = vowelSubstring.slice(1);
-                i++;
-            }
-            j++;
-        }
-    }
     return ans;
 }

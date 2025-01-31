1de58790a39f64030d2dd41e873dfb2de3f414db
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-01-31 21:56:03 +0530

    reverse string with stack

---
+++ Stacks/1.ReverseStringWithStack.ts
@@ -0,0 +1,20 @@
+import { Stack } from "./Impementation/Stack_using_Class";
+
+function revString(s: string): string {
+    let n: number = s.length;
+
+    if(n <= 1) return s;
+
+    let reversedString: string = '';
+    let stack = new Stack(n);
+    s.split('').forEach((char) => stack.push(Number(char)));
+
+    while(stack.currentSize > 0) {
+        stack.pop();
+        reversedString += stack.poppedElement;
+    }
+
+    return reversedString;
+}
+
+console.log(revString('1234')) // "4321" 

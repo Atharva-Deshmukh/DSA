2bdeb6763dcd925f5fd6576c194d43280e017961
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-02-01 16:53:37 +0530

    circular queue implementation

--- Queues/Implementation/3.CircularQueue.ts
+++ Queues/Implementation/3.CircularQueue.ts
@@ -1,21 +1,68 @@
-/* REFER THE IMAGE TO KNOW WHY DO WE USE A CICRULAR QUEUE 
+/* 
+A Circular Queue is another way of implementing a normal queue where the last element of the queue 
+is connected to the first element of the queue forming a circle.
+
+It is also called ring buffer.
+
+REFER THE IMAGE TO KNOW WHY DO WE USE A CICRULAR QUEUE 
 Since, in a normal queue, which is an array under the hood, we cannot add further elements
-even though we have space at front, since our pointer has reached the end,
+even though we have space at front, since our rear/tail pointer has reached the end,
 we use circular queue here
 
 We use array of specific size here since we can add a new element by first removing any 
 existing one, so Its MEMORY EFFICIENT
+
+                                 size = 3
+
+                              frnt   rear
+                                |      | 
+                            <-- [1, 2, 3] <--
+
+                                  frnt rear
+                                    |  | 
+                            <-- [ , 2, 3] <-- pop()
+
+                                rear  frnt
+                                 |     | 
+                            <-- [4, 2, 3] <-- push(4) rather than shifting all elements to left by 1
+
+                            So, whenever rear === size-1, rear = 0;
+
+------------------------------------------------------------------------------------------------------------
+                                    ANOTHER CONCEPT: circular indexes
+                            We will do front++ and rear++ using circular indexing
+
+           0  1  2
+let arr = [1, 2, 3] n = 3
+
+0 = (0 % 3)
+1 = (1 % 3)
+2 = (2 % 3)
+
+now, i = 3 in circular array = index 0
+3 = (3 % 3) = 0
+4 = (4 % 3) = 1
+
+
+So (rear + 1) % totalSize
+   (front + 1) % totalSize
+
+
+
 */
 
 class CircularQueue {
     private cqueue: number[];
-    private size: number;
+    private totalSize: number;
     private currentSize: number;
+    public dequeuedElement: number
+
+    // we need these two pointers to track the rear and front so that we can simulate circular effect
     private front: number;
     private rear: number;
 
     constructor(size: number) {
-        this.size = size;
+        this.totalSize = size;
 
         this.cqueue = [];
         this.front = -1;   // denotes index here
@@ -23,51 +70,54 @@ class CircularQueue {
         this.currentSize = 0;
     }
 
-    enqueue(val: number) {
+    enQueue(val: number): boolean {
+        if (this.isFull()) return false; 
+        
+        // If queue is empty, initialize front
+        if (this.front === -1) this.front = 0;
 
-        // agar cqueue full nhi hai to
-        if(this.currentSize !== this.size) {
-            
-            // agar queue ka size max tak pohoch gya, to rear ko 0th index, uski khudki jagah 
-            // pe leke aajao, rear jab yaha aayga to now, hum circular way se handle kar sakte
-            //Note that rear is an index, hence size-1
-            // LAST WALE KO 0 PE LAANE SE CIRCULAR EFFECT AATA HAI
-            if(this.rear === this.size-1) this.rear = 0;
-            else this.rear++;
+        // Move rear pointer circularly
+        this.rear = (this.rear + 1) % this.totalSize;
 
-            this.cqueue[this.rear] = val;
-            this.currentSize++;
+        // insert val at updated rear pointer
+        this.cqueue[this.rear] = val;
 
-            // if first element hai, to front ko bhi 0 pe leke aao jaha tab rear hoga
-            if(this.front === -1) this.front = this.rear;
-        }
-        else {
-            alert('QUEUE IS FULL');
-        }
+        // update the size
+        this.currentSize++;
+
+        return true;
     }
 
-    dequeue() {
-        if(this.currentSize !== 0) {
-            // delete current element at front (front = 0) at start when we did this.front = this.rear
-            this.cqueue[this.front] = null;
-
-            // element remove honey ke baad front jo hai vo array me aage aage aata hai, 
-            // but if uska index max size barabar hai, to usko 0 pe laao in order to reuse the space
-            // LAST WALE KO 0 PE LAANE SE CIRCULAR EFFECT AATA HAI
-            if(this.front === this.size-1) this.front = 0;
-            else this.front++;
-            this.currentSize--;
-        }
-        else {
-            // reset everything for an empty queue
-            this.front = -1;
-            this.rear = -1;
-            alert('QUEUE IS EMPTY');
-        }
-    }
+    deQueue(): boolean {
+        if (this.isEmpty()) return false;
+         
+         // Remove the front element
+         this.cqueue[this.front] = null;
+ 
+         // move pointers
+         // If only one element was present, reset front & rear
+         if (this.front === this.rear) {
+             this.front = -1;
+             this.rear = -1;
+         } else {
+             this.front = (this.front + 1) % this.totalSize; // Move front circularly
+         }
+ 
+         this.currentSize--;
+         return true;
+         
+     }
 
     display() {
         console.warn(this.cqueue);
+    }
+
+    private isFull() {
+        return (this.totalSize === this.currentSize);
+    }
+
+    private isEmpty() {
+        return (this.currentSize === 0);
     }
 };
 

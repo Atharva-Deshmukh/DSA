9df3e33a2c22fcb05053c37ebbc9caab7852c86f
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-02-03 20:23:31 +0530

    stack implementation

---
+++ Stacks/Impementation/stack_using_class_string.ts
@@ -0,0 +1,38 @@
+export class Stack {
+    private currentSize: number;
+    private totalSize: number;
+    private stack: string[];
+    public poppedElement: string;
+
+    constructor(size: number) {
+        this.currentSize = 0;
+        this.totalSize = size;
+        this.stack = [];
+        this.poppedElement = '';
+    }
+
+    push(element: string) {
+        if(this.isFull()) console.log('STACK FULL');
+        else {
+            this.stack[this.currentSize] = element;
+            this.currentSize++;
+        }
+    }
+
+    pop() {
+        if(this.isEmpty()) console.log('STACK EMPTY');
+        else {
+            this.poppedElement = this.stack[this.currentSize - 1];
+            this.currentSize--;
+            this.stack.length = this.currentSize;
+        }
+    }
+
+    isFull() {
+        return (this.currentSize === this.totalSize)
+    }
+
+    isEmpty() {
+        return (this.currentSize === 0)
+    }
+} 

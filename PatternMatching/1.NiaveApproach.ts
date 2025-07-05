33bcdb587ab3ea4d149085b22f3055f3b91c4f89
Atharva Deshmukh <atharvadeshmukh2103@gmail.com>
2025-07-05 11:27:05 +0530

    Naive approach

--- PatternMatching/1.NiaveApproach.ts
+++ PatternMatching/1.NiaveApproach.ts
@@ -12,6 +12,12 @@ j = 0 on pat
 Iterate both simultaneously and get the matches, when matches fail, start from next index in txt
 include corner cases based on pat.length and txt.length 
 
+Ex, if txtLen = 5 and patLen = 2:
+i goes from 0 to 3 (i.e., 0, 1, 2, 3)
+That's 4 iterations, which is 5 - 2 + 1 = 4
+
+And for every i, j compares full patLen, hence multiply by patLen
+
 TC: O((txtLen - patLen + 1) * patLen) 
 SC: O(1) -> no extra space */
 
@@ -28,7 +34,8 @@ function niavePatternSearchApproach(txt: string, pat: string): number[] {
     // in the last iteration, we can accomodate the pattern length so that last window can be compared 
     for(let i = 0; i <= (txtLen - patLen); i++) {
         
-        // for each i, compare whole window using (i + j) => i is fixed and 0 <= j < patLen, so we can note i if pattern matches
+        // for each i, compare whole window using (i + j) => i is fixed and 0 <= j < patLen, 
+        // so we can note i if pattern matches
         let j: number = 0;
         while((j < patLen) && (txt[i + j] === pat[j])) j++;
 

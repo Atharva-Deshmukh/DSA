1768ac732cc8bdd296147eceb8df1ad55d1dc15f
Atharva Deshmukh <deshmukhatharva2103@gmail.com>
2025-10-26 13:58:46 +0530

    diamond pattern

--- PatternPrograms/8.DiamondPattern.ts
+++ PatternPrograms/8.DiamondPattern.ts
@@ -9,122 +9,118 @@
       * * *         |         1 2 3         |         A B C
         *           |           1           |           A
 
-Pattern: (n = 5)
-- No of rows = (2n - 1)
-- Thought process:
+n = 9
 
-         for(row = 1; row <= (2n - 1); row++) {
-            if(row <= 5) // sidha printing -- already did full pyramid q.3
-            if(row > 5)  // ulta printing
-         }
+row1 -> 4 spaces + 1 char + 4 spaces
+row2 -> 3 spaces + 3 char + 3 spaces
+row3 -> 2 spaces + 5 char + 2 spaces
+row4 -> 1 spaces + 7 char + 1 spaces
+row5 -> 0 spaces + 9 char + 0 spaces
 
-
-(row <= n) 
-    row1 --> 4 spaces + 1 stars + 4 spaces
-    row2 --> 3 spaces + 3 stars + 3 spaces
-    row3 --> 2 spaces + 5 stars + 2 spaces
-    row4 --> 1 spaces + 7 stars + 1 spaces
-    row5 --> 0 spaces + 9 stars + 0 spaces
-
-(row > n) 
-    row6 --> 1 spaces + 7 stars + 1 spaces
-    row7 --> 2 spaces + 5 stars + 2 spaces
-    row8 --> 3 spaces + 3 stars + 3 spaces
-    row9 --> 4 spaces + 1 stars + 4 spaces
-
-    each row will have ((2*row) - 1) stars 
-    Max stars = when row = n
-
-    For inverted pyramid:
-    row6: 
-       (6 - 1) = 1 = (totalSpacesInRow/2), we can get stars from here
+row6 -> 1 spaces + 7 char + 1 spaces
+row7 -> 2 spaces + 5 char + 2 spaces
+row8 -> 3 spaces + 3 char + 3 spaces
+row9 -> 4 spaces + 1 char + 4 spaces
 
 */
 
-function diamondPatternStars(n: number) {
-    let totalRows: number = (2 * n) - 1;
-    let maxStarsPossible: number = (2 * n) - 1;
-    let patRow: string = '';
+function diamondPatternNumber(n: number): void {
 
-    for(let row = 1; row <= totalRows; row++) {
-        patRow = '';
-        if(row <= n) {
-            let starsInRow: number = ((2 * row) - 1);
-            let spacesInRowEachSide: number = Math.floor((maxStarsPossible - starsInRow) / 2);
+    const maxChars: number = n;  // here maxChars = n only
+    const mid: number = Math.floor(n / 2) + 1;  // for n = 9, mid = 5
 
-            patRow += '  '.repeat(spacesInRowEachSide);
-            patRow += ' *'.repeat(starsInRow);
-            patRow += '  '.repeat(spacesInRowEachSide);
+    for (let i = 1; i <= n; i++) {
+        let patRow: string = '';
 
-            console.log(patRow);
-        } else {
-            let spacesInRowEachSide: number = (row - n);
-            let starsInRow: number = (maxStarsPossible - (2 * spacesInRowEachSide));
-
-            patRow += '  '.repeat(spacesInRowEachSide);
-            patRow += ' *'.repeat(starsInRow);
-            patRow += '  '.repeat(spacesInRowEachSide);
-
-            console.log(patRow);
+        if (i <= mid) {
+            const charsInThisRow: number = (2 * i) - 1;
+            const spacesEachSide: number = Math.floor((maxChars - charsInThisRow) / 2);
+            patRow = '  '.repeat(spacesEachSide) + '* '.repeat(charsInThisRow) + '  '.repeat(spacesEachSide);
         }
+        else {
+            const differenceOfiFromMid = i - mid;  // since i > mid
+            const spacesEachSide: number = differenceOfiFromMid;
+            const charsInThisRow: number = maxChars - (2 * spacesEachSide);
+            patRow = '  '.repeat(spacesEachSide) + '* '.repeat(charsInThisRow) + '  '.repeat(spacesEachSide);
+        }
+        console.log(patRow);
     }
+
 }
 
 function diamondPatternNumber(n: number) {
-    let totalRows: number = (2 * n) - 1;
-    let maxNumsPossible: number = (2 * n) - 1;
-    let patRow: string = '';
+    const maxChars: number = n;  // here maxChars = n only
+    const mid: number = Math.floor(n / 2) + 1;  // for n = 9, mid = 5
 
-    for(let row = 1; row <= totalRows; row++) {
-        patRow = '';
-        if(row <= n) {
-            let numsInRow: number = ((2 * row) - 1);
-            let spacesInRowEachSide: number = Math.floor((maxNumsPossible - numsInRow) / 2);
+    for (let i = 1; i <= n; i++) {
+        let patRow: string = '';
 
-            patRow += '  '.repeat(spacesInRowEachSide);
-            for(let num = 1; num <= numsInRow; num++) patRow += ' ' + num;
-            patRow += '  '.repeat(spacesInRowEachSide);
+        if (i <= mid) {
+            const charsInThisRow: number = (2 * i) - 1;
+            const spacesEachSide: number = Math.floor((maxChars - charsInThisRow) / 2);
 
-            console.log(patRow);
-        } else {
-            let spacesInRowEachSide: number = (row - n);
-            let numsInRow: number = (maxNumsPossible - (2 * spacesInRowEachSide));
+            // numbers
+            for (let j = 1; j <= charsInThisRow; j++) {
+                patRow += j + ' ';
+            }
 
-            patRow += '  '.repeat(spacesInRowEachSide);
-            for(let num = 1; num <= numsInRow; num++) patRow += ' ' + num;
-            patRow += '  '.repeat(spacesInRowEachSide);
-
-            console.log(patRow);
+            // now add spaces
+            patRow = '  '.repeat(spacesEachSide) + patRow + '  '.repeat(spacesEachSide);
         }
+        else {
+            const differenceOfiFromMid = i - mid;  // since i > mid
+            const spacesEachSide: number = differenceOfiFromMid;
+            const charsInThisRow: number = maxChars - (2 * spacesEachSide);
+
+            // numbers
+            for (let j = 1; j <= charsInThisRow; j++) {
+                patRow += j + ' ';
+            }
+
+            // now add spaces
+            patRow = '  '.repeat(spacesEachSide) + patRow + '  '.repeat(spacesEachSide);
+        }
+        console.log(patRow);
     }
 }
 
 function diamondPatternChar(n: number, startChar: string = 'A') {
-    let totalRows: number = (2 * n) - 1;
-    let maxCharsPossible: number = (2 * n) - 1;
-    let patRow: string = '';
-    let startCharIndex: number = startChar.charCodeAt(0) - 1;
+    const maxChars: number = n;  // here maxChars = n only
+    const mid: number = Math.floor(n / 2) + 1;  // for n = 9, mid = 5
 
-    for(let row = 1; row <= totalRows; row++) {
-        patRow = '';
-        if(row <= n) {
-            let charsInRow: number = ((2 * row) - 1);
-            let spacesInRowEachSide: number = Math.floor((maxCharsPossible - charsInRow) / 2);
+    for (let i = 1; i <= n; i++) {
+        let patRow: string = '';
 
-            patRow += '  '.repeat(spacesInRowEachSide);
-            for(let charNum = 1; charNum <= charsInRow; charNum++) patRow += ' ' + String.fromCharCode(startCharIndex + charNum);
-            patRow += '  '.repeat(spacesInRowEachSide);
+        if (i <= mid) {
+            const charsInThisRow: number = (2 * i) - 1;
+            const spacesEachSide: number = Math.floor((maxChars - charsInThisRow) / 2);
 
-            console.log(patRow);
-        } else {
-            let spacesInRowEachSide: number = (row - n);
-            let charsInRow: number = (maxCharsPossible - (2 * spacesInRowEachSide));
+            const startingChar: string = 'A';
 
-            patRow += '  '.repeat(spacesInRowEachSide);
-            for(let charNum = 1; charNum <= charsInRow; charNum++) patRow += ' ' + String.fromCharCode(startCharIndex + charNum);
-            patRow += '  '.repeat(spacesInRowEachSide);
+            // characters
+            for (let j = 1; j <= charsInThisRow; j++) {
+                patRow += String.fromCharCode(startingChar.charCodeAt(0) + (j - 1)) + ' ';
+            }
 
-            console.log(patRow);
+            // now add spaces
+            patRow = '  '.repeat(spacesEachSide) + patRow + '  '.repeat(spacesEachSide);
         }
+        else {
+            const differenceOfiFromMid = i - mid;  // since i > mid
+            const spacesEachSide: number = differenceOfiFromMid;
+            const charsInThisRow: number = maxChars - (2 * spacesEachSide);
+
+            const startingChar: string = 'A';
+
+            // numbers
+            for (let j = 1; j <= charsInThisRow; j++) {
+                patRow += String.fromCharCode(startingChar.charCodeAt(0) + (j - 1)) + ' ';
+            }
+
+            // now add spaces
+            patRow = '  '.repeat(spacesEachSide) + patRow + '  '.repeat(spacesEachSide);
+        }
+        console.log(patRow);
     }
+
 }

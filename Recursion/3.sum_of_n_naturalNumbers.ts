4834748de8be876077ba70d6c94b1bd41c0ff7b6
Atharva Deshmukh <deshmukhatharva2103@gmail.com>
2025-11-10 21:58:20 +0530

    Sum of first n natural numbers

---
+++ Recursion/3.sum_of_n_naturalNumbers.ts
@@ -0,0 +1,30 @@
+/* Thinking:
+   - Go down and down, keep adding the sum and passing it down
+   - In the last, return it
+
+*/
+
+function parameterisedRecursion_Sum(n: number, sum: number): void {
+
+    if(n < 1) {
+        console.log(sum);
+        return;
+    }
+    
+    parameterisedRecursion_Sum(n - 1, sum + n);
+}
+
+parameterisedRecursion_Sum(3, 0);
+parameterisedRecursion_Sum(5, 0);
+
+/* TREE
+
+    F(3, 0)
+        \
+       F(2, 3)
+          \
+         F(1, 5)
+            \
+           F(0, 6)
+
+ */

d167be85cd8d66e9d09f434f01f87f1f8eb52b96
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-01-13 19:50:46 +0530

    Position of only set bit

--- BitManipulation/05.positionOfOnlySetBit.ts
+++ BitManipulation/05.positionOfOnlySetBit.ts
@@ -1,15 +1,6 @@
-/*Find position of the only set bit
+/* Find position of the only set bit
 
-Way 1: (NOT WORKING) we can think of using direct formula
-
-console.log(Math.floor(Math.log(1)));  // 0001  Log's output: 0
-console.log(Math.floor(Math.log(2)));  // 0010  Log's output: 0
-console.log(Math.floor(Math.log(4)));  // 0100  Log's output: 1
-console.log(Math.floor(Math.log(8)));  // 1000  Log's output: 2
-
-hence directly using Math.log() is slightly unreliable
-
-Way 2: Create a mask by normal Iteration
+Create a mask by normal Iteration
 
 1 set bit === a power of two 
 

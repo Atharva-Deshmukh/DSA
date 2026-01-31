80eb94bd4994d2a0e17c82445d4b45f81b70c80f
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-01-31 13:38:00 +0530

    Transpose of matrix

---
+++ Mathematical/Matrix/01.TransposeOfMatrix.ts
@@ -0,0 +1,25 @@
+/* 
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+
+
+In place algo only works for square matrix */
+
+function transpose(matrix: number[][]): number[][] {
+    const m: number = matrix.length;
+    const n: number = matrix[0].length;
+
+    // transpose result matrix = n * m not m * n
+    let res: number[][] = new Array(n).fill(0).map((ele) => new Array(m).fill(0));
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            res[j][i] = matrix[i][j];
+        }
+    }
+
+    return res;
+}

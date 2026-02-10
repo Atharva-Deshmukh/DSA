049c91696f2c586c22d57d65a6135264e2a7acb3
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-02-10 20:58:38 +0530

    Sum of subarray minimum and sum of subarray maximums

--- Stacks/11.SumOfSubarrayMinimum.ts
+++ Stacks/11.SumOfSubarrayMinimum.ts
@@ -64,7 +64,9 @@ then contribution = (count * a[i])
 
  Better to store the next smaller element indices and the previous smaller element indices
 
-Edge cases:
+                                                Edge cases:
+                                                -----------
+
 - if no smaller element on right, 
   [1, 2, 3, 4] for 2 
    0  1  2  3
@@ -78,16 +80,15 @@ Edge cases:
    push index as -1, minus adjusts accordingly -> 2 - (-1) = 3
         
 
-
-One little edge case to keep in mind
+- One little edge case to keep in mind
     
-         0  0
+         0  1
 arr[] = [1, 1]
 nse   =  2  2      -> n because we didn't find one on the right
 pse   =  -1 -1     -> -1 because we didn't find one on the left
 
-but, for arr[0], subarrays considered = [1], [1, 1]
-and, for arr[1], subarrays considered = [1], [1, 1] 
+but, for arr[0], subarrays where 1st 1 is minimum = [1], [1, 1]
+and, for arr[1], subarrays where 2nd 1 is minimum = [1], [1, 1] 
 [1, 1] is twice!!
 
 so, either include this once in any of the two,
@@ -203,4 +204,112 @@ function sumSubarrayMins(a: number[]): number {
     }
 
     return minSum;
+};
+
+////////////////////////////////////// SUM OF SUBARRAY MAXIMUM //////////////////////////////////
+
+class myStack {
+    private currentSize: number;
+    private stack: number[];
+    private minElement: number;
+
+    constructor() {
+        this.currentSize = 0;
+        this.stack = [];
+        this.minElement = Number.MAX_SAFE_INTEGER;
+    }
+
+    /* if stack is empty, simply push the current element without modifing and update minimumEle*/
+    push(element: number): void {
+        if (this.isEmpty()) {
+            this.stack[this.currentSize] = element;
+            this.minElement = element;
+        } else {
+            if (element < this.minElement) {
+                this.stack[this.currentSize] = 2 * element - this.minElement;
+                this.minElement = element;
+            } else {
+                this.stack[this.currentSize] = element;
+            }
+        }
+        this.currentSize++;
+    }
+
+    pop(): void {
+        if (this.isEmpty()) return;
+
+        let topElement = this.stack[this.currentSize - 1];
+
+        if (topElement < this.minElement) {
+            this.minElement = 2 * this.minElement - topElement;
+        }
+
+        this.currentSize--;
+        this.stack.length = this.currentSize;
+
+        // if after poppint, stack becomes empty, reset the minimum element
+        if (this.isEmpty()) {
+            this.minElement = Number.MAX_SAFE_INTEGER;
+        }
+    }
+
+    top(): number {
+        if (this.isEmpty()) return -1;
+
+        let topElement = this.stack[this.currentSize - 1];
+
+        // If the stored value is less than minElement, the actual value is minElement.
+        return topElement < this.minElement ? this.minElement : topElement;
+    }
+
+    getMin(): number {
+        return this.isEmpty() ? -1 : this.minElement;
+    }
+
+    isEmpty(): boolean {
+        return this.currentSize === 0;
+    }
+}
+
+function sumSubarrayMaxs(a: number[]): number {
+    const n: number = a.length;
+
+    if (n === 0) return 0;
+    if (n === 1) return a[0];
+
+    let maxSum: number = 0;
+
+    let prevGreaterElementIndex: number[] = Array(n);
+    let nextGreaterElementIndex: number[] = Array(n);
+
+    let nextGreaterStack = new myStack();
+    for (let i = (n - 1); i >= 0; i--) {
+        while ((!nextGreaterStack.isEmpty()) && (a[nextGreaterStack.top()] <= a[i])) nextGreaterStack.pop();
+
+        nextGreaterElementIndex[i] = (nextGreaterStack.isEmpty()) ? n : nextGreaterStack.top();
+
+        nextGreaterStack.push(i);
+    }
+
+
+    let prevGreaterStack = new myStack();
+    for (let i = 0; i < n; i++) {
+        
+                                                   // same corner case applied
+        while ((!prevGreaterStack.isEmpty()) && (a[prevGreaterStack.top()] < a[i])) prevGreaterStack.pop();
+
+        prevGreaterElementIndex[i] = (prevGreaterStack.isEmpty()) ? -1 : prevGreaterStack.top();
+
+        prevGreaterStack.push(i);
+    }
+
+    for (let i = 0; i < n; i++) {
+        const contributionsFromLeft: number = (i - prevGreaterElementIndex[i]);
+        const contributionsFromRight: number = (nextGreaterElementIndex[i] - i);
+        const totalContribution: number = (contributionsFromLeft * contributionsFromRight) * a[i];
+
+        maxSum = maxSum + totalContribution;
+    }
+
+    return maxSum;
 };

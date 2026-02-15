48b92e318c38cbed378396d0ecdfd1816a048396
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-02-15 15:35:34 +0530

    Trapping rainwater problem

--- Stacks/10.TrappingRainwaterProblem.ts
+++ Stacks/10.TrappingRainwaterProblem.ts
@@ -1,10 +1,4 @@
-/* 
-Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
-Output: 6
-Explanation: The above elevation map (black section) is represented by array 
-[0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
-
-Constraints:
+/* Constraints:
 
 n == height.length
 1 <= n <= 2 * 10^4

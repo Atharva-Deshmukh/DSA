8fab17346039b4d2fdc471e4386b77e08d547f01
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-06-10 21:33:16 +0530

    Transpose of a matrix

--- Mathematical/Matrix/01.TransposeOfMatrix.ts
+++ Mathematical/Matrix/01.TransposeOfMatrix.ts
@@ -1,25 +1,42 @@
 /* 
-Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
-Output: [[1,4,7],[2,5,8],[3,6,9]]
+Input: matrix = 
+            [
+             [1,2,3],
+             [4,5,6],
+             [7,8,9]
+            ]
 
-Input: matrix = [[1,2,3],[4,5,6]]
-Output: [[1,4],[2,5],[3,6]]
+Output:     [
+             [1,4,7],
+             [2,5,8],
+             [3,6,9]
+            ]
+
+Input: matrix = [
+                 [1,2,3],
+                 [4,5,6]
+                ]                     NOTE: Create answer matrix of n * m instead of m * n, otherwise there will be overflow
+
+Output:         [
+                 [1,4],
+                 [2,5],
+                 [3,6]
+                ]
 
 
-Logic: indexwise visualisation
+Way-1: indexwise visualisation
 
-00 01 02
-10 11 12
-20 21 22
+                            00 01 02              00 10 20
+                            10 11 12      -->     01 11 21  
+                            20 21 22              02 12 22
 
-00 10 20
-01 11 21
-02 12 22
+                            00 01                 20 10 00
+                            10 11         -->     21 11 01
+                            20 21
 
-Way-1:
-- Use extra space to store Answer. 
-  NOTE: Create answer matrix of n * m instead of m * n, otherwise there will be overflow
-- Iterate whole input matrix, and keep putting transposeMat[j][i] = input[i][j];
+                        
+- Create a new matrix of (n * m)
+- transposeMat[j][i] = input[i][j];  // input matrix is of the dimensions m * n only, hence keep i, j for it
 
 TC: O(m * n)
 SC: O(m * n)
@@ -27,9 +44,7 @@ SC: O(m * n)
 Way-2: In-place
 - NOTE: In place algo only works for square matrix (n * n)
 - Notice that diagonals are in-place unaffected, only elements around diagonals are swapped
-- We have a trick here -> i = 0, j = (i + 1) - MEMORISE this trick
-
-*/
+- We have a trick here -> i = 0, j = (i + 1) - MEMORISE this trick  */
 
 function transposeExtraSpace(matrix: number[][]): number[][] {
     const m: number = matrix.length;
@@ -47,7 +62,7 @@ function transposeExtraSpace(matrix: number[][]): number[][] {
     return res;
 }
 
-/* Memorise this index trick */
+/* Memorise this index trick - Only for square matrix*/
 function transposeInPlace(matrix: number[][]): number[][] {
     const m: number = matrix.length;
     const n: number = matrix[0].length;

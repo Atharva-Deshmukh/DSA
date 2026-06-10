c059859419a131671dd005b623ded2f27e59cd93
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-06-10 21:38:33 +0530

    Rotate matrix

--- Mathematical/Matrix/02.RotateMatrixNinetyDegree.ts
+++ Mathematical/Matrix/02.RotateMatrixNinetyDegree.ts
@@ -1,38 +1,22 @@
 /* 
 Leetcode: 48. Rotate Image (Rotate clockwise  90 Degrees)
 
-00 01 02
-10 11 12
-20 21 22
+Index-wise visualisation
 
-20 10 00
-21 11 01
-22 12 02
+00 01 02                 20 10 00
+10 11 12       -->       21 11 01
+20 21 22                 22 12 02
 
-Shifts Index-wise
-00 -> 02
-01 -> 12
-02 -> 22
-
-10 -> 01
-11 -> 11
-12 -> 21
-
-20 -> 00
-21 -> 10
-22 -> 20
 
 Pattern -> 
+         i' = (n - 1) - j
+         j' = i
 
-new i = j
-new j = ((n - 1) - i)
-
-Way-1: 
+Way-1: Go index-wise
 - Create a new matrix to store answer and iterate original matrix
   newMatrix[j][((n - 1) - i)] = inputMatrix[i][j]
 
-Way-2: In-place. Memorise this trick
-- There are some observations
+Way-2: In-place Tricks: Transpose original matrix --> reverse each row
 
 1 2 3        7 4 1
 4 5 6   -->  8 5 2
@@ -69,9 +53,7 @@ function rotateClockWise(matrix: number[][]): void {
     }
 } 
 
-/*  Rotate anti-clockwise 90 Degrees
-
-TRICK TO MEMORISE: reverse each row and then take transpose
+/*  Rotate anti-clockwise 90 Degrees: reverse each row --> transpose
 
 1 2 3        3 6 9
 4 5 6   -->  2 5 8

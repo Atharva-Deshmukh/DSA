aac8ed32ba67fb9d74755b70fa6d2827419bdeca
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-07-01 21:11:24 +0530

    lower bound

--- Binary_Search/Pattern1_StandardBinarySearch/02.LowerBound.ts
+++ Binary_Search/Pattern1_StandardBinarySearch/02.LowerBound.ts
@@ -33,3 +33,48 @@ function lowerBound_BS(arr: number[], key: number): number {
 
     return ans;
 }
+
+/* Related Question -> Search Insert Position
+
+You are given a sorted array of distinct integers and a target value, 
+return the index if the target is found. If not, return the index where 
+it would be if it were inserted in order.
+
+
+Input: nums = [-1,0,2,4,6,8], target = 5
+Output: 4
+
+Input: nums = [-1,0,2,4,6,8], target = 10
+Output: 6
+
+Sorted array -> Binary search here.
+Looks like We have to essentially find lower bound here.
+*/
+
+function searchInsert(a, key) {
+        const n = a.length;
+
+        let low = 0;
+        let high = (n - 1);
+        let ans = Number.MAX_SAFE_INTEGER;
+
+        /* corner cases - when elements can go out of the array itself */
+
+        /* If smallest element of the sorted array is greater than the key*/
+        if(a[0] > key) return 0; 
+
+        /* If largest element of the sorted array is less than the key*/
+        if(a[n - 1] < key) return n;
+        
+        while(low <= high) {
+            const mid = low + Math.floor((high - low) / 2);
+            
+            if(a[mid] >= key) {
+                ans = (mid < ans)? mid: ans;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+
+        return ans;
+    }

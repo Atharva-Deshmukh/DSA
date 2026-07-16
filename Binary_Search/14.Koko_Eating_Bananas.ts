ea26c80f56e83beb6469662db2fc347b14a8608d
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-07-16 21:13:45 +0530

    Koko eating bananas

--- Binary_Search/14.Koko_Eating_Bananas.ts
+++ Binary_Search/14.Koko_Eating_Bananas.ts
@@ -1,57 +1,73 @@
-/* Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. 
-The guards have gone and will come back in h hours.Koko can decide her bananas-per-hour eating speed of k. 
+/* Leetcode 875: Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. 
+The guards have gone and will come back in h hours. Koko can decide her bananas-per-hour eating speed of k. 
 Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, 
 she eats all of them instead and will not eat any more bananas during this hour.
 Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
 Return the minimum integer k such that she can eat all the bananas within h hours.
 
-
 Input: piles = [3,6,7,11],      h = 8   Output: 4
 
 Input: piles = [30,11,23,4,20], h = 5   Output: 30
 
-Logic (Linear): 
-- Note that koko can move to next pile only if she finishes current pile in that hour
+                                                Way-1: Brute Force
+                                                ------------------
+
+- Each hour, she chooses some pile of bananas and eats k bananas from that pile.
+  Hence, koko can move to next pile only if she finishes current pile in that hour.
 - She decides her eating speed k and iterates on that pile[] 
-- she will have to try for k from 1 to INT_MAX and get the total time for each i, 
+- So, she will have to try for k = [1 to INT_MAX] and get the total time for each k, 
   if totalTime < h, that is the ans
-  ex: 
-  let k = 1
-    piles[] = [3, 6, 7, 11]
-time taken  = [3, 6, 7, 11] --> (using Math.ceil(arr[i]/k)), ceil because if arr[i] < k, that hour will be wasted, koko 
-                                                             will take full hour even though she finishes eating
 
-total time  = 3 + 6 + 7 + 11 > h, NOT ACCEPTABLE 
+  let's say koko decided the rate = k
+  time taken = Math.ceil(piles[i] / k)
+
+  ceil because if (arr[i] < k), that hour will be wasted, koko will take full hour even though 
+  she finishes eating
+
+  Ceil and floor don't follow standard rounding rules, they just consider next and previous integer values
+
+| Input | ceil() (Up) | floor() (Down) |
+|  ---  |  ---------- |  ------------- |
+| 4.1   | 5           | 4              |
+| 4.9   | 5           | 4              |
+| -4.1  | -4          | -5             |
+| -4.9  | -4          | -5             |
+
+
+k = 1:
+    piles[] = [3, 6, 7, 11] and h = 8
+    time taken (ceil values) = [3, 6, 7, 11] ==> 3 + 6 + 7 + 11 > h ==> NOT ACCEPTABLE 
   
-ex: let k = 2
+k = 2:
     piles[] = [3, 6, 7, 11]
-time taken  = [3, 6, 7, 11] --> (using Math.ceil(arr[i]/k)), ceil because if arr[i] < k, that hour will be wasted, koko 
-                                                             will take full hour even though she finishes eating
+    time taken  = [2, 3, 4, 6] ==> total time  > h, NOT ACCEPTABLE 
 
-total time  > h, NOT ACCEPTABLE 
-
-ex: let k = 3
+k = 3:
     piles[] = [3, 6, 7, 11]
-time taken  = [1, 2, 3, 4] --> (using Math.ceil(arr[i]/k)), ceil because if arr[i] < k, that hour will be wasted, koko 
-                                                             will take full hour even though she finishes eating
+    time taken  = [1, 2, 3, 4] > h, NOT ACCEPTABLE 
 
-total time  > h, NOT ACCEPTABLE 
-
-ex: let k = 4
+k = 4:
     piles[] = [3, 6, 7, 11]
-time taken  = [1, 2, 2, 3] --> (using Math.ceil(arr[i]/k)), ceil because if arr[i] < k, that hour will be wasted, koko 
-                                                             will take full hour even though she finishes eating
-
-total time  <= h, ACCEPTABLE and MINIMUM value 
+    time taken  = [1, 2, 2, 3] ==> total time  <= h, ACCEPTABLE and MINIMUM value 
   
 
-TC: O(INT_MAX * arr.length), it will give TLE!!
+TC: O(INT_MAX * arr.length), Obviously, it will give TLE!!
 SC: O(1)
 
-BS Logic:
-- Note that we try k from 1 --- INT_MAX, but that can also be 1 --- Math.max(...arr)
-- whenever we have ans after a particular range, we can use BS. Refer sqrt using BS Q
-- Determine the range for BS, here it is [1....Math.max(...arr)]
+                                            Way-2: Binary Search
+                                            --------------------
+
+- Though Process: we try k from [1 -- Math.max(...arr)]
+- whenever we stop getting ans after particular limit and we wan't max or min value, use BS
+
+  So, our BS space = [1 -- Math.max(...arr)]
+
+  We are calculating (piles[i] / k) --> as k increases, time decreases and lower the time, 
+                                                        more possible the answer
+
+  hence we will get something like [X, X, X, X, X, ✓, ✓, ✓, ✓, ✓]
+                                                   |
+                                                  ans
 
 
 TC: O(arr.length * log2(MAX_ARR_ELEMENT))
@@ -90,10 +106,7 @@ function BS_Solution(piles: number[], h: number): number {
             high = mid - 1;                         // explore further smaller values
         }
 
-        // if eating 3 bananas takes 8 hrs say, then eating 2 and 1 bananas will obviously take more time,
-        // So, eliminate left half
-        else if(totalTime > h) low = mid + 1; 
-
+        else low = mid + 1; 
     }
 
     return ans;

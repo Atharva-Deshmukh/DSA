b421c729cfd7a65586cd1ffa30f6e074824d8083
Atharva Deshmukh <atharva.deshmukh@siemens.com>
2026-07-16 20:35:08 +0530

    Mth root of N

--- Binary_Search/03.Pattern3_BinarySearchOnAnswer/02.MthRootOfN.ts
+++ Binary_Search/03.Pattern3_BinarySearchOnAnswer/02.MthRootOfN.ts
@@ -20,16 +20,10 @@ SOLUTION:
 - Why do we need to calculate even the whole power, we can create a function like calc(base, N)
   keep multiplying till we reach >= M and return a flag based on that
 
-  keep a local variable ans = 1 initially, iterate from 1 to M(the max possibility)
-  at every i, ans = ans * i
-  Best explained via code ;)
-
 TC: O(N * log(M))
 SC: O(1) */
 
-// this function stops power calculation when result > M, helps when M or N is too large (10 ^ 9)
-// returns custom flags to denote the situations
-// This is also called binary exponentiation method
+/* This is also called binary exponentiation method */
 function calcPow(N: number, M: number, mid: number): number {
     let ans: number = 1;
     for(let i = 1; i <= N; i++) {
@@ -49,11 +43,12 @@ function NthRootOfM_UsingBS(N: number, M: number): number {
     let high: number = M;
 
     while(low <= high) {
-        let mid: number = low + Math.floor((high - low) / 2);
+        const mid: number = low + Math.floor((high - low) / 2);
+        const power: number = calcPow(N, M, mid);
 
-        if(calcPow(N, M, mid) === 1) return mid;
-        else if(calcPow(N, M, mid) === 2) high = mid - 1;
-        else if(calcPow(N, M, mid) === 0) low = mid + 1;
+        if(power === 1) return mid;
+        else if(power === 2) high = mid - 1;
+        else if(power === 0) low = mid + 1;
     }
 
 
